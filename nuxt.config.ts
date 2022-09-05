5ae899e64ed1a70088af42486e854aca9cfa58dc
daver987 <daveyalexander@outlook.com>
2022-09-05 11:44:07 -0400

    Add: Configuration for cloudflare pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'cloudflare',
+    preset: 'cloudflare_pages',
   },
 
   modules: [
