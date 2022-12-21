97033a872834e20f1533a8e8d19cd03e2b6312bc
daver987 <daveyalexander@outlook.com>
2022-12-20 20:49:39 -0500

    Add: nuxt edge version

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
   <QPage padding>
-  <div class="grid grid-cols-1 md:grid-cols-2">
-  <div class="col-span-1"></div>
-  <div class="col-span-1">
-    <ClientOnly>
-
-  <QuasarForm />
-    </ClientOnly>
-  </div>
-  </div>
-
-
+    <div class="grid grid-cols-1 md:grid-cols-2">
+      <div class="col-span-1"></div>
+      <div class="col-span-1">
+        <ClientOnly>
+          <QuasarForm />
+        </ClientOnly>
+      </div>
+    </div>
   </QPage>
 </template>
 
