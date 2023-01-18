0f7fb26402be019a34eefd80931236a6ada459e3
daver987 <daveyalexander@outlook.com>
2023-01-17 23:41:45 -0500

    Add: svg gradiant background to cloudflare images

--- components/App/AppHeader.vue
+++ components/App/AppHeader.vue
@@ -32,7 +32,9 @@ defineProps({
 <template>
   <header
     class="relative min-h-screen overflow-hidden"
-    style="background-image: url('/icons/gradient-background.svg')"
+    style="
+      background-image: url('https://imagedelivery.net/9mQjskQ9vgwm3kCilycqww/c2f2c5f0-7a8d-4617-e4aa-d81156a40600/4200');
+    "
   >
     <AppNavigation />
     <BaseContainer
